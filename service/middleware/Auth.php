bd096eb0705dffac96f3b36ab52b018752885d20
moctarafendi <moctarafendi@gmail.com>
2018-09-13 21:59:52 +0700

    rapihin indent lagi

--- service/middleware/Auth.php
+++ service/middleware/Auth.php
@@ -1,25 +1,19 @@
 <?php
-
-class Auth{
+class Auth
+{ 
 	public function __invoke($request, $response, $next)
-    {
-      $headerValueArray = $request->getHeader('Authorization');
-      $apiToken = explode(' ', $headerValueArray[0]);
-      $db = Database::connect();
-      $query = $db->prepare("SELECT * FROM userx WHERE token=:token");           
-      $query->execute([
-            "token" => $apiToken[1],
-      ]);
-
-      $user = $query->fetch(\PDO::FETCH_OBJ);
-      if($user){ 
-	      	$response = $next($request, $response);
-	      	return $response;
-      }else{
-	         return $response->withJson([
-	              "message"=> "Token Salah"
-	         ]);
-      }
-        
-    }
+	{
+		$headerValueArray = $request->getHeader('Authorization');
+		$apiToken = explode(' ', $headerValueArray[0]);
+		$db = Database::connect();
+		$query = $db->prepare("SELECT * FROM userx WHERE token=:token");
+		$query->execute(["token" => $apiToken[1], ]);
+		$user = $query->fetch(PDO::FETCH_OBJ);
+		if ($user){
+			$response = $next($request, $response);
+			return $response;
+		}else{
+			return $response->withJson(["message" => "Token Salah"]);
+		}
+	}
 }
