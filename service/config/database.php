4c6cc5bee22223342aa34a7396d78a920aef7986
insanrizky <muhamad.rizky@bizzy.co.id>
2018-09-18 08:53:21 +0700

    ENV Prod

--- service/config/database.php
+++ service/config/database.php
@@ -1,10 +1,10 @@
 <?php
 class Database {
-	private static $host 	    = "localhost";
-	private static $db 		    = "it_expo2";
+	private static $host 	    = "192.168.4.91";
+	private static $db 		    = "itexpo_2018";
 	private static $username	= "root";
-    private static $password 	= "";
-    private static $driver 	= "mysql";
+    private static $password 	= "B248b2a";
+    private static $driver 	    = "mysql";
 
 
     public static function connect(){
