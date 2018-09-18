03d799909036fd0f8fab89eb412473a7a562d48a
moctarafendi <moctarafendi@gmail.com>
2018-09-18 22:22:39 +0700

    add upload foto and ktm api,  add new_column sql

--- service/index.php
+++ service/index.php
@@ -1,6 +1,7 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+        
 
 
 require './vendor/autoload.php';
@@ -8,6 +9,9 @@ require './config/config.php';
 require './config/database.php';
 
 $app = new \Slim\App(['settings' => $config]);
+
+
+
 require './controllers/CommonController.php';
 require './controllers/AuthController.php';
 require './controllers/EventController.php';
--- service/routes.php
+++ service/routes.php
@@ -7,6 +7,8 @@ $app->post('/event/register', 'EventController::registerEvent')->add( new Auth()
 $app->get('/event/user/{id}', 'EventController::getEvent')->add( new Auth() );
 $app->post('/event/submit/{id}', 'EventController::submitEvent')->add( new Auth() );
 $app->post('/logout', 'AuthController::logout')->add( new Auth() );
+$app->post('/upload/ktm', 'AuthController::uploadKtm')->add( new Auth() );
+$app->post('/upload/foto', 'AuthController::uploadFoto')->add( new Auth() );
 $app->get('/hello/{name}', 'CommonController::home');
 $app->post('/save', 'CommonController::save');
 $app->get('/users', 'CommonController::getUsers')->add( new Auth() );
